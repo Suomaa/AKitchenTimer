3f208aba719b28fbec45d98c2ff8b9bbd836209b
Suomaa <a.antomonov@mail.ru>
2017-11-02 18:29:17 +0300

    + Added a button start timer

--- app/src/main/java/fi/suomaafrontieroy/akitchentimer/MainActivity.java
+++ app/src/main/java/fi/suomaafrontieroy/akitchentimer/MainActivity.java
@@ -3,12 +3,17 @@ package fi.suomaafrontieroy.akitchentimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTimer;
+    private NumberPicker hoursPicker;
+    private NumberPicker minutesPicker;
+    private NumberPicker secondsPicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
         mTimer = (TextView) findViewById(R.id.textTimer);
 
-        NumberPicker hoursPicker = (NumberPicker) findViewById(R.id.hoursPicker);
+        hoursPicker = (NumberPicker) findViewById(R.id.hoursPicker);
         hoursPicker.setMaxValue(24);
         hoursPicker.setMinValue(0);
 
-        NumberPicker minutesPicker = (NumberPicker) findViewById(R.id.minutesPicker);
-        minutesPicker.setMaxValue(60);
+        minutesPicker = (NumberPicker) findViewById(R.id.minutesPicker);
+        minutesPicker.setMaxValue(59);
         minutesPicker.setMinValue(0);
 
-        NumberPicker secondsPicker = (NumberPicker) findViewById(R.id.secondsPicker);
-        secondsPicker.setMaxValue(60);
+        secondsPicker = (NumberPicker) findViewById(R.id.secondsPicker);
+        secondsPicker.setMaxValue(59);
         secondsPicker.setMinValue(0);
 
-        new CountDownTimer(20000, 1000) {
+    }
+
+    public void CreateTimer(int timer) {
+        new CountDownTimer(timer, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                String tick = "To the finish: " + millisUntilFinished / 1000;
+                String tick = "Time remaining: " + millisUntilFinished / 1000;
                 mTimer.setText(tick);
             }
 
@@ -43,4 +51,11 @@ public class MainActivity extends AppCompatActivity {
                 .start();
     }
 
+    public void clickButtonStart(View view) {
+        int hours = hoursPicker.getValue();
+        int minutes = minutesPicker.getValue();
+        int seconds = secondsPicker.getValue();
+        int timer = (hours * 3600 + minutes * 60 + seconds) * 1000;
+        CreateTimer(timer);
+    }
 }
